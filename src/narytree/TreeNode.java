0c5b590111b2fe93bfd2d7f7e8782656047c7144
T-Lind <tiernanxklindauer@gmail.com>
2023-02-16 10:52:09 -0600

    tree example run

--- src/narytree/TreeNode.java
+++ src/narytree/TreeNode.java
@@ -6,7 +6,13 @@ public class TreeNode<T> {
     private T value;
     private ArrayList<TreeNode<T>> childNodes;
 
+    public TreeNode(){
+        childNodes = new ArrayList<>();
+        this.value = null;
+    }
+
     public TreeNode(T value){
+        childNodes = new ArrayList<>();
         this.value = value;
     }
 
@@ -23,6 +29,10 @@ public class TreeNode<T> {
     public void addChild(TreeNode<T> newNode){
         childNodes.add(newNode);
     }
+    public void addChild(T value){
+        childNodes.add(new TreeNode<T>(value));
+    }
+
 
     public TreeNode<T> getChild(int idx){
         return childNodes.get(idx);
@@ -30,5 +40,9 @@ public class TreeNode<T> {
 
     public TreeNode<T> removeChild(int idx){
         return childNodes.remove(idx);
+    }
+
+    public String toString(){
+        return value.toString();
     }
 }
